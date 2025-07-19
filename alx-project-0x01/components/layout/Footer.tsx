d5a8ce374254eda3e14675df8e82c1fc01499d59
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-19 16:33:36 +0100

    Create Footer.tsx

---
+++ alx-project-0x01/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-200 text-center py-4 mt-auto">
+      <p className="text-gray-600">&copy; 2025 Daily Contents. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
