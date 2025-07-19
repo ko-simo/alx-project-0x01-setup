a96ae83a8a1db1b1941eeefcfc5b8d7938a882f4
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-19 16:57:56 +0100

    Update index.tsx

--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -1,14 +1,19 @@
-import Header from "@/components/layout/Header";
+'use client';
 
-const Home: React.FC = () => {
+import Header from '@/components/layout/Header';
+import Footer from '@/components/layout/Footer';
+
+export default function Home() {
   return (
     <>
       <Header />
-      <div className="flex justify-center items-center h-screen">
-        <h1 className="text-7xl font-thin">Welcome Page</h1>
-      </div>
+      <main className="flex flex-col items-center justify-center min-h-screen py-10">
+        <h1 className="text-4xl font-bold mb-6">Welcome to our Application!</h1>
+        <p className="text-lg text-center max-w-xl">
+          Navigate through posts and users using the navigation links above.
+        </p>
+      </main>
+      <Footer />
     </>
   );
-};
-
-export default Home;
+}
