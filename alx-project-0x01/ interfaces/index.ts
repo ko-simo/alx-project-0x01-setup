28df4dd4bb50eb0135a174c05e8aefaecf580d20
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-19 16:56:32 +0100

    Update index.ts

--- alx-project-0x01/ interfaces/index.ts
+++ alx-project-0x01/ interfaces/index.ts
@@ -25,5 +25,5 @@ export interface UserData {
 export interface UserModalProps {
   isOpen: boolean;
   onClose: () => void;
-  onAddUser: (user: UserData) => void;
+  onSubmit: (user: UserData) => void;
 }
